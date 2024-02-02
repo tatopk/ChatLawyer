4814dcf80fb0c48348d1a80d7d3e79e5fe2f51e5
tatopk <t.rekhviashvili@mima-poker.com>
2024-02-02 14:07:46 +0400

    testing

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
+
+interface formData{
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  // constructor(private http: HttpClient) { }
 
+  // onSubmit(formValue: formData){
+  //   formValue.phone = "000000000"
+  //   this.http.post('http://localhost:8000/api/register', formValue).subscribe((data) => {
+  //     console.log(data);
+  //   });
+  // }
 }
