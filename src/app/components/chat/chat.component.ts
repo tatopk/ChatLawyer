7557502d97002f9565bd8d6820917005c531a8cf
tatopk <t.rekhviashvili@mima-poker.com>
2024-02-20 17:26:21 +0400

    changing chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 
 export class ChatComponent {
+  chats = [
+    { name: 'Ana', time: '4 day', message: 'Let\'s discuss this', image: 'assets/Ana.JPG', active: false },
+    { name: 'Nick', time: '05:30 am', message: 'Good bye', image:'assets/Nick.JPG', active: false },
+    { name: 'George', time: '3 day',  message: 'Thank you', image:'assets/George.JPG', active: false }
+  ];
 
+  messages = [
+    { image: "assets/Ana.JPG", message: "Blah blah blah", time: "05:25 am",  sender: "other" },
+    { image: "assets/Profile.png", message: "Exactly, even more Blah blah blah", time: "05:25 am",  sender: "user"  }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.chats[0].active = true;
+  }
+
+  toggleActive(item: any) {
+    this.chats.forEach(chatItem => {
+      if (chatItem !== item) {
+        chatItem.active = false;
+      }
+    });
+
+    item.active = !item.active;
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { ChatComponent } from '../components/chat/chat.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ChatComponent, HeaderComponent, FooterComponent],
+  imports: [ChatComponent, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  onAskClick() {
+    console.log('Ask clicked');
+  }
 }
